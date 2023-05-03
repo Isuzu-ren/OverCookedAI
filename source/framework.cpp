530cabd7c3d5a6b59d84467ae5f0cd2ae8812b93
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-03 20:36:43 +0800

    compile

--- source/framework.cpp
+++ source/framework.cpp
@@ -7,6 +7,71 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <deque>
+#include <cctype>
+#include <cmath>
+
+// enum Status
+// {
+//     FREE,
+//     GO_TO_INGREDIENT,
+//     TAKING_INGREDIENT_TO_COOK,
+//     COOKING,
+//     TAKING_INGREDIENT_TO_PLATE,
+//     TAKING_PLATE_TO_SERVICEWINDOWS,
+//     TAKING_DIRTYPLATE_TO_SINK,
+//     WASHING,
+//     COLLISION_AVOIDENCE
+// };
+// static Status p0status, p1status;
+
+enum DirectionInteract
+{
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+    CHECKED
+};
+enum TypeInteract
+{
+    TAKE,
+    INTERACT
+};
+
+enum TypeStep
+{
+    GO_TO_INGREDIENT,
+    TAKING_INGREDIENT_TO_COOK,
+    COOKING,
+    TAKING_INGREDIENT_TO_PLATE,
+    TAKE_UP_PLATE,
+    TAKING_PLATE_TO_SERVICEWINDOWS,
+    TAKING_DIRTYPLATE_TO_SINK,
+    WASHING,
+    COLLISION_AVOIDENCE
+};
+struct Step
+{
+    int desx, desy;      // 终点坐标
+    bool descheck;       // 终点是否已确定
+    DirectionInteract d; // 交互方向
+    TypeInteract ta;     // 交互方式
+    TypeStep ts;         // 任务内容
+};
+
+struct Task
+{
+    Step stp[10];  // 每步需要完成的任务
+    int stpsum;    // 总步数
+    int completed; // 完成数量
+};
+
+std::string IngredientName[5] =
+    {"fish", "rice", "kelp"};
+
+std::string ContainerName[5] =
+    {"Pan", "Pot", "Plate", "DirtyPlates"};
 
 /* 按照读入顺序定义 */
 int width, height;
@@ -24,6 +89,222 @@ struct Player Players[2 + 5];
 int entityCount;
 struct Entity Entity[20 + 5];
 int remainFrame, Fund;
+
+struct Plate
+{
+    int x, y;
+    bool used;
+};
+int platenum = 0;
+Plate platearr[20];
+
+// 找到一个可以和位于坐标(y,x)的实体或特殊地砖交互的地点，相关信息记录在步骤stp中
+void CheckInteractPos(Step &stp, const int x, const int y)
+{
+    if ((x > 0) && (!isupper(Map[y][x - 1])) && (getTileKind(Map[y][x - 1]) == TileKind::Floor))
+    {
+        stp.desx = x - 1;
+        stp.desy = y;
+        stp.d = RIGHT;
+    }
+    else if ((x < width - 1) && (!isupper(Map[y][x + 1])) && (getTileKind(Map[y][x + 1]) == TileKind::Floor))
+    {
+        stp.desx = x + 1;
+        stp.desy = y;
+        stp.d = LEFT;
+    }
+    else if ((y > 0) && (!isupper(Map[y - 1][x])) && (getTileKind(Map[y - 1][x]) == TileKind::Floor))
+    {
+        stp.desx = x;
+        stp.desy = y - 1;
+        stp.d = DOWN;
+    }
+    else if ((y < height - 1) && (!isupper(Map[y + 1][x])) && (getTileKind(Map[y + 1][x]) == TileKind::Floor))
+    {
+        stp.desx = x;
+        stp.desy = y + 1;
+        stp.d = UP;
+    }
+    else
+    {
+        assert(0);
+    }
+}
+
+// double dis[(20 + 2) * (20 + 2)][(20 + 2) * (20 + 2)] = {};
+// int pre[(20 + 2) * (20 + 2)][(20 + 2) * (20 + 2)] = {};
+
+int xservicewindows, yservicewindows;
+int xsink, ysink;
+
+// 初始化一些信息，确定盘子的总数和各自的坐标，确定服务台坐标和洗盘子水槽坐标
+void init_map()
+{
+    for (int i = 0; i < entityCount; i++)
+    {
+        for (auto it : Entity[i].entity)
+        {
+            if (it == "Plate")
+            {
+                platearr[platenum].used = false;
+                platearr[platenum].x = Entity[i].x;
+                platearr[platenum].y = Entity[i].y;
+                platenum++;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < height * width; i++)
+    {
+        if ((!isupper(Map[i / width][i % width])) && (getTileKind(Map[i / width][i % width]) == TileKind::ServiceWindow))
+        {
+            xservicewindows = i % width;
+            yservicewindows = i / width;
+            break;
+        }
+    }
+    for (int i = 0; i < height * width; i++)
+    {
+        if ((!isupper(Map[i / width][i % width])) && (getTileKind(Map[i / width][i % width]) == TileKind::Sink))
+        {
+            xsink = i % width;
+            ysink = i / width;
+            break;
+        }
+    }
+    // for (int i = 0; i < height; i++)
+    // {
+    //     for (int j = 0; j < width; j++)
+    //     {
+    //         if ((!isupper(Map[i][j])) && (getTileKind(Map[i][j]) == TileKind::ServiceWindow))
+    //         {
+    //             xservicewindows = i;
+    //             yservicewindows = j;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    //     const double sq2 = sqrt(2);
+    //     for (int i = 0; i < height; i++)
+    //     {
+    //         for (int j = 0; j < width; j++)
+    //         {
+    //             int cur = i * width + j;
+    //             if ((!isupper(Map[i][j])) && (getTileKind(Map[i][j]) == TileKind::Floor))
+    //             {
+    //                 if ((i - 1 >= 0) && (j - 1 >= 0) &&
+    //                     (getTileKind(Map[i - 1][j - 1]) == TileKind::Floor) &&
+    //                     (getTileKind(Map[i - 1][j]) == TileKind::Floor) &&
+    //                     (getTileKind(Map[i][j - 1]) == TileKind::Floor))
+    //                 {
+    //                     dis[cur][cur - width - 1] = sq2;
+    //                 }
+    //                 if ((i - 1 >= 0) &&
+    //                     (getTileKind(Map[i - 1][j]) == TileKind::Floor))
+    //                 {
+    //                     dis[cur][cur - width] = 1;
+    //                 }
+    //             }
+    //         }
+    //     }
+}
+
+Task totalOrderParseTask[20 + 5];
+// 该函数只负责最初解析订单所需要做的事，目前一个订单对应一个任务，但之后可能修改为更适合合作完成的分解小任务，其解析的任务中含有未确定的目标，例如并未确定盘子要在何处取，当任务真正被分配然后执行的时候才会确定
+Task ParseOrder(const struct Order &order)
+{
+    Task task;
+    task.stpsum = 0;
+    // 取食材，并放到盘子里
+    for (auto it : order.recipe)
+    {
+        for (int i = 0; i < IngredientCount; i++)
+        {
+            if (Ingredient[i].name == it)
+            {
+                CheckInteractPos(task.stp[task.stpsum], Ingredient[i].x, Ingredient[i].y);
+                task.stp[task.stpsum].descheck = true;
+                task.stp[task.stpsum].ta = TAKE;
+                task.stp[task.stpsum].ts = GO_TO_INGREDIENT;
+                task.stpsum++;
+                task.stp[task.stpsum].descheck = false;
+                task.stp[task.stpsum].ta = TAKE;
+                task.stp[task.stpsum].ts = TAKING_INGREDIENT_TO_PLATE;
+                task.stpsum++;
+                break;
+            }
+        }
+    }
+    // 拿起盘子
+    task.stp[task.stpsum].descheck = false;
+    task.stp[task.stpsum].ta = TAKE;
+    task.stp[task.stpsum].ts = TAKE_UP_PLATE;
+    task.stpsum++;
+    // 将盘子送往服务台
+    CheckInteractPos(task.stp[task.stpsum], xservicewindows, yservicewindows);
+    task.stp[task.stpsum].descheck = true;
+    task.stp[task.stpsum].ta = TAKE;
+    task.stp[task.stpsum].ts = TAKING_PLATE_TO_SERVICEWINDOWS;
+    task.stpsum++;
+    task.completed = 0;
+    return task;
+}
+
+const double epsilon = 1e-6;
+// ret 低四位表示移动方向 0001-右 0010-左 0100-下 1000-上
+int Move(const double px, const double py, const int dx, const int dy)
+{
+    int ret = 0;
+    if (px <= double(dx + 0.35))
+    {
+        ret |= 0x1;
+    }
+    else if (px >= double(dx + 0.65))
+    {
+        ret |= 0x2;
+    }
+    if (py <= double(dy + 0.35))
+    {
+        ret |= 0x4;
+    }
+    else if (py >= double(dy + 0.65))
+    {
+        ret |= 0x8;
+    }
+    return ret;
+}
+
+int RunningTaskSum = 0;
+std::deque<Task> deq;
+Task ptask[2 + 2];
+
+// ret 低6位表示行动方案 低4位 0001-右 0010-左 0100-下 1000-上 低56位 00-Move 01-Interact 10-PutOrPick
+int Action(const int op)
+{
+    assert(op < 2);
+    Task &ct = ptask[op];
+    int ret = Move(Players[op].x, Players[op].y, ct.stp[ct.completed].desx, ct.stp[ct.completed].desy);
+    if (ret != 0)
+        return ret;
+    if (ct.stp[ct.completed].ta == INTERACT)
+        ret |= 0x10;
+    else if (ct.stp[ct.completed].ta == TAKE)
+        ret |= 0x20;
+    else
+        assert(0);
+    if (ct.stp[ct.completed].d == RIGHT)
+        ret |= 0x01;
+    else if (ct.stp[ct.completed].d == LEFT)
+        ret |= 0x02;
+    else if (ct.stp[ct.completed].d == DOWN)
+        ret |= 0x04;
+    else if (ct.stp[ct.completed].d == UP)
+        ret |= 0x08;
+    else
+        assert(0);
+    return ret;
+}
 
 void init_read()
 {
@@ -88,6 +369,18 @@ void init_read()
         ss >> Entity[i].x >> Entity[i].y >> s;
         Entity[i].entity.push_back(s);
     }
+
+    init_map();
+    for (int i = 0; i < totalOrderCount; i++)
+    {
+        totalOrderParseTask[i] = ParseOrder(totalOrder[i]);
+    }
+    deq.clear();
+    ptask[0].stpsum = 0;
+    ptask[1].stpsum = 0;
+    ptask[0].completed = 0;
+    ptask[1].completed = 0;
+    RunningTaskSum = 0;
 }
 
 bool frame_read(int nowFrame)
@@ -104,7 +397,7 @@ bool frame_read(int nowFrame)
     if (std::cin.rdbuf()->in_avail() > 0)
     {
         std::cerr << "Warning: skipping frame " << nowFrame
-             << " to catch up with the game" << std::endl;
+                  << " to catch up with the game" << std::endl;
         return true;
     }
     ss >> s;
@@ -153,7 +446,7 @@ bool frame_read(int nowFrame)
             if (s == ";" || s == ":" || s == "@" || s == "*")
                 continue;
 
-            /* 
+            /*
                 Todo: 其他容器
             */
             if (s == "Plate")
@@ -195,8 +488,8 @@ bool frame_read(int nowFrame)
                 assert(s == "/");
                 break;
             }
-            
-            /* 
+
+            /*
                 Todo: 其他容器
             */
             if (s == "Plate")
