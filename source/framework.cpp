0ef8869144eb73b21eda6aa9e7edd11c33ac728b
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-23 11:51:14 +0800

    compile

--- source/framework.cpp
+++ source/framework.cpp
@@ -1086,28 +1086,28 @@ void ParseOrder()
             stsk.stpsum++;
             if (pantime >= pottime)
             {
-                CheckInteractPos(stsk.stp[stsk.stpsum], xpan, ypan);
-                stsk.stp[stsk.stpsum].ta = TAKE;
-                stsk.stp[stsk.stpsum].ts = TAKING_PLATE_TO_PAN;
-                stsk.stp[stsk.stpsum].product = ingTask[TaskPan[TaskPan.size() - 1]].stp[ingTask[TaskPan[TaskPan.size() - 1]].stpsum - 1].product;
-                stsk.stpsum++;
                 CheckInteractPos(stsk.stp[stsk.stpsum], xpot, ypot);
                 stsk.stp[stsk.stpsum].ta = TAKE;
                 stsk.stp[stsk.stpsum].ts = TAKING_PLATE_TO_POT;
                 stsk.stp[stsk.stpsum].product = ingTask[TaskPot[TaskPot.size() - 1]].stp[ingTask[TaskPot[TaskPot.size() - 1]].stpsum - 1].product;
+                stsk.stpsum++;
+                CheckInteractPos(stsk.stp[stsk.stpsum], xpan, ypan);
+                stsk.stp[stsk.stpsum].ta = TAKE;
+                stsk.stp[stsk.stpsum].ts = TAKING_PLATE_TO_PAN;
+                stsk.stp[stsk.stpsum].product = ingTask[TaskPan[TaskPan.size() - 1]].stp[ingTask[TaskPan[TaskPan.size() - 1]].stpsum - 1].product;
                 stsk.stpsum++;
             }
             else
             {
-                CheckInteractPos(stsk.stp[stsk.stpsum], xpot, ypot);
-                stsk.stp[stsk.stpsum].ta = TAKE;
-                stsk.stp[stsk.stpsum].ts = TAKING_PLATE_TO_POT;
-                stsk.stp[stsk.stpsum].product = ingTask[TaskPot[TaskPot.size() - 1]].stp[ingTask[TaskPot[TaskPot.size() - 1]].stpsum - 1].product;
-                stsk.stpsum++;
                 CheckInteractPos(stsk.stp[stsk.stpsum], xpan, ypan);
                 stsk.stp[stsk.stpsum].ta = TAKE;
                 stsk.stp[stsk.stpsum].ts = TAKING_PLATE_TO_PAN;
                 stsk.stp[stsk.stpsum].product = ingTask[TaskPan[TaskPan.size() - 1]].stp[ingTask[TaskPan[TaskPan.size() - 1]].stpsum - 1].product;
+                stsk.stpsum++;
+                CheckInteractPos(stsk.stp[stsk.stpsum], xpot, ypot);
+                stsk.stp[stsk.stpsum].ta = TAKE;
+                stsk.stp[stsk.stpsum].ts = TAKING_PLATE_TO_POT;
+                stsk.stp[stsk.stpsum].product = ingTask[TaskPot[TaskPot.size() - 1]].stp[ingTask[TaskPot[TaskPot.size() - 1]].stpsum - 1].product;
                 stsk.stpsum++;
             }
             CheckInteractPos(stsk.stp[stsk.stpsum], xservicewindows, yservicewindows);
