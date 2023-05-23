4fd0c1d345e6efb434fb2042274e695182e9637d
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-23 10:50:04 +0800

    compile

--- source/framework.cpp
+++ source/framework.cpp
@@ -18,7 +18,7 @@
 // #define WASHPLATESHIFT
 #define TRUEMOVE
 // #define SIMPLEBRAKECONTROL
-#define NEOBRAKECONTROL
+// #define NEOBRAKECONTROL
 
 struct Step
 {
@@ -674,10 +674,10 @@ bool CollisionDetection(const int fret)
         return false;
     if (((fret & 0x30) == 0) &&
         (((fret >> 6) & 0x30) == 0) &&
-        (Players[0].X_Velocity < 0.2) &&
-        (Players[0].Y_Velocity < 0.2) &&
-        (Players[1].X_Velocity < 0.2) &&
-        (Players[1].Y_Velocity < 0.2) &&
+        (Players[0].X_Velocity < 0.4) &&
+        (Players[0].Y_Velocity < 0.4) &&
+        (Players[1].X_Velocity < 0.4) &&
+        (Players[1].Y_Velocity < 0.4) &&
         (((Players[0].x - Players[1].x) * (Players[0].x - Players[1].x) + (Players[0].y - Players[1].y) * (Players[0].y - Players[1].y)) < playercollisiondistance))
         return true;
     else
