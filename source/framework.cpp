654cf4a38c2eda275574ba9a2da130e0003004ae
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-26 09:17:08 +0800

    compile

--- source/framework.cpp
+++ source/framework.cpp
@@ -589,9 +589,9 @@ int Move(const int op, const int dx, const int dy)
         return 0x10;
 #endif
 #ifdef TRUEMOVE
-    if ((pnum == dnum) &&
-        (Players[op].X_Velocity * Players[op].X_Velocity + Players[op].Y_Velocity * Players[op].Y_Velocity > 4))
-        return 0x10;
+    // if ((pnum == dnum) &&
+    //     (Players[op].X_Velocity * Players[op].X_Velocity + Players[op].Y_Velocity * Players[op].Y_Velocity > 4))
+    //     return 0x10;
     int next = Dijkstra(dnum, pnum);
     if (next == -1)
     {
